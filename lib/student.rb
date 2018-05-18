b06fded2779b72dd48496565e54f5c8009ab418f
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-18 16:45:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 #require_relative "../config/environment.rb"
 class Student
   attr_accessor  :name, :grade
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
 
   def self.new_from_db(row)
     new_student = self.new
